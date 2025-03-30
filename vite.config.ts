5942ab2589eb0aedec96ce4d423e294d34997f8e
Rashida Strozier <rashida.strozier@gmail.com>
2025-03-30 14:13:47 -0400

    successfully added tailwind css

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react' // Import the React plugin
+import tailwindcss from '@tailwindcss/vite' // Import the Tailwind CSS plugin
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(), 
+    tailwindcss(), 
+  ],
 })
